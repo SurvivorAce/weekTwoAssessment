4444f9399efaa42a9dffe90e64a3b93f4dfe0b27
Anderson Nguyen <andersonnguyen126@gmail.com>
2015-04-29 12:01:40 -0700

    Assessment Thingy!

---
+++ main.js
@@ -0,0 +1,9 @@
+var thingy = document.getElementById( "iwao" );
+ 
+iwao.addEventListener( "click", function( event ) {
+    alert( "Hey you clicked on me" );
+}, false );
+
+$( ".dan" ).on( "click", function() {
+    $( "div.dan" ).hide();
+});
